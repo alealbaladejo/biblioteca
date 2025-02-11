81244d6ea5aa104af2f22cc68b7a634937ab2369
Alejandro Albaladejo <alealbaladejo29s@gmail.com>
2025-02-11 13:08:28 +0000

    Cambios subidos

---
+++ build/borrar-iniciar.sh
@@ -0,0 +1,3 @@
+docker-compose down -v
+docker build -t alealbaladejo/biblioteca:v2 .
+docker-compose up -d
--- build/docker-entrypoint.sh
+++ build/docker-entrypoint.sh
@@ -3,4 +3,5 @@ while ! mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST}  -e ";" ; do
 	sleep 1
 done	
 mysql -u ${DB_USER} -p${DB_PASS} -h ${DB_HOST} ${DB_NAME} < /opt/biblioteca.sql
+sed -i '/<Directory \/var\/www\/>/,/<\/Directory>/ s/AllowOverride None/AllowOverride All/' /etc/apache2/apache2.conf
 apache2ctl -D FOREGROUND
